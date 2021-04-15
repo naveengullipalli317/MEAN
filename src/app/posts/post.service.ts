9f9c5948233606ede737380bd306f01687a059e6
I am naveen Gullipalli <naveengullipalli317@gmail.com>
2021-04-15 14:02:35 +0200

    Adding angular to posting the data and getting the data from the server side

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -26,7 +26,12 @@ export class PostService {
 }
   addPost(tittle: string, content:string){
   const post: Post = {id: null, tittle:tittle, content: content};
-  this.posts.push(post);
-  this.postsUpdated.next([...this.posts]);
+  this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+  .subscribe((responseData)=>{
+    console.log(responseData.message);
+    this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
+  });
+
 }
 }
