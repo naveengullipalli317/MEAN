94b549baa8011edbe8d9cb34bdee59f5674d964a
I am naveen Gullipalli <naveengullipalli317@gmail.com>
2021-04-17 11:03:15 +0200

    Adding routing to the componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { FormsModule} from '@angular/forms';
+
+
 import { MatInputModule} from '@angular/material/input';
 import { MatCardModule} from '@angular/material/card';
 import { MatButtonModule} from '@angular/material/button';
