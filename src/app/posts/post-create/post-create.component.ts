108fae8157c30f4147729fbba79ccc3207f3394d
I am naveen Gullipalli <naveengullipalli317@gmail.com>
2021-04-19 17:15:04 +0200

    Adding an image preview to the frontend

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -15,6 +15,7 @@ export class PostCreateComponent implements OnInit {
 enteredTittle="";
 enteredContent="";
 form: FormGroup;
+imagePreview: string;
 
 private mode = 'create';
 private postId: string;
@@ -59,8 +60,11 @@ onImagePicked(event: Event) {
   const file = (event.target as HTMLInputElement).files[0];
   this.form.patchValue({image:file});
   this.form.get('image').updateValueAndValidity();
-  console.log(file);
-  console.log(this.form);
+  const reader = new FileReader();
+  reader.onload = () => {
+    this.imagePreview = reader.result as string;
+  };
+  reader.readAsDataURL(file);
 }
 
 onSavePost() {
