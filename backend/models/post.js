60a06adc584e360ed5b1b0ab9061818b43314537
I am naveen Gullipalli <naveengullipalli317@gmail.com>
2021-06-24 16:44:09 +0200

    Authorization: Adding a reference to a model, by creating creator in the post.js

--- backend/models/post.js
+++ backend/models/post.js
@@ -4,9 +4,10 @@ const mongoose = require('mongoose');
 const postSchema = mongoose.Schema({
   tittle: {type: String, required: true},
   content: {type: String, required: true},
-  imagePath: {type: String, required: true}
+  imagePath: {type: String, required: true},
+  creator: { type: mongoose.Schema.Types.ObjectId,  ref: "User", required: true}
 });
 
 
 //turning the definition into model
-module.exports = mongoose.model('Post', postSchema);
+module.exports = mongoose.model("Post", postSchema);
