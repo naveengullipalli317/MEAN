6d19d0c933c2ba4dc6da37a384dc714592b7f91b
I am naveen Gullipalli <naveengullipalli317@gmail.com>
2021-07-05 14:01:12 +0200

    Creating an angular material module

---
+++ src/app/angular-material.module.ts
@@ -0,0 +1,24 @@
+import { MatInputModule} from '@angular/material/input';
+import { MatCardModule} from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatExpansionModule} from '@angular/material/expansion';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatDialogModule} from '@angular/material/dialog';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+exports:[
+  MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatDialogModule
+]
+})
+
+export class  AngularMaterialModule{}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,7 @@ import { ReactiveFormsModule} from '@angular/forms';
 import { FormsModule} from '@angular/forms';
 
 
-import { MatInputModule} from '@angular/material/input';
-import { MatCardModule} from '@angular/material/card';
-import { MatButtonModule} from '@angular/material/button';
-import { MatToolbarModule} from '@angular/material/toolbar';
-import { MatExpansionModule} from '@angular/material/expansion';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import { MatDialogModule} from '@angular/material/dialog';
+
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -26,6 +19,7 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './error/error.component';
+import { AngularMaterialModule } from './angular-material.module';
 
 
 @NgModule({
@@ -46,16 +40,8 @@ import { ErrorComponent } from './error/error.component';
     ReactiveFormsModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule,
     HttpClientModule,
-    MatPaginatorModule,
-    MatProgressSpinnerModule,
-    MatDialogModule
-
+    AngularMaterialModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
